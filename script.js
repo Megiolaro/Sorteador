1ba6e8478f22326ef94952f154bcd6f73992913c
Megiolaro <giancarlosmegiolaro@gmail.com>
2023-08-05 19:06:40 -0300

    Valor apresentado na propria tela

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function numberSort(){
 
     const min = Math.ceil(document.querySelector(".first-input").value)
     const max = Math.floor(document.querySelector(".second-input").value)
+    const numeroGerado = document.querySelector(".numero")
 
     const result = Math.floor(Math.random() * (max - min + 1) + min)
 
-    alert(result)
+    numeroGerado.innerHTML = result
 
 }
 
